0ec58dfda6cd6a65d94c52db96de827e9a492023
Dmitriy Klebanov <dmitriy.klebanov@outlook.com>
2020-05-20 17:25:25 +0300

    added balance_detail test

--- finance/tests/test_views.py
+++ finance/tests/test_views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.test import RequestFactory
-
 from django.urls import reverse
 
 import pytest
 
-from finance.views import BalancesListView
+from finance.models import Balance
+from finance.views import BalancesListView, BalanceDetailView
 
 
 @pytest.mark.django_db
@@ -20,3 +21,16 @@ class TestListView:
         response = BalancesListView.as_view()(request)
 
         assert response.status_code == 200
+
+    def test_balance_detail_view(self):
+        user1 = User.objects.create(username='tmp1')
+        user2 = User.objects.create(username='tmp2')
+        balance = Balance.objects.create(account=user2, name='42', amount=34)
+
+        url = reverse('balance_detail', kwargs={'pk': balance.id})
+
+        request = RequestFactory().get(url)
+        request.user = user1
+
+        with pytest.raises(PermissionDenied):
+            BalanceDetailView.as_view()(request, pk=balance.id)
