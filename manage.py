c547d6087248c55e58ca1a0a328bb31413e3e919
Dmitriy Klebanov <dmitriy.klebanov@outlook.com>
2020-05-20 16:05:24 +0300

    moved on windows

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'financial_manager.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'financial_manager.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
